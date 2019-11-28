bb752f5ad7a460f21852fa6205899d43ab181478
NicEastvillage <nicoesterby@gmail.com>
2019-11-28 11:30:27 +0100

    Use generated graph loaded from csv.

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"encoding/binary"
-	"encoding/hex"
+	"bufio"
+	"encoding/csv"
 	"fmt"
 	"html/template"
-	"math/rand"
+	"io"
 	"net/http"
-	"regexp"
+	"os"
+	"strconv"
 )
 
 type Link struct {
@@ -15,49 +16,83 @@ type Link struct {
 	Title string
 }
 
-var idRegex = regexp.MustCompile("^[abcdefABCDEF0123456789]{16}$")
 var tmpl = template.Must(template.ParseFiles("page.html"))
+var linkMap = make(map[int][]Link)
 
 const domain = "http://localhost:8080"
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	if idRegex.MatchString(id) {
-		bytes, err := hex.DecodeString(id)
-		if err != nil {
-			panic("failed to decode")
-		}
-
-		num := binary.BigEndian.Uint64(bytes)
-
-		links := make([]Link, 64)
-		shuffle := rand.Perm(64)
-
-		for i := uint64(0); i < 64; i += 1 {
-			newNum := num ^ (uint64(0x01) << i)
-			newId := fmt.Sprintf("%016x", newNum)
-			links[shuffle[i]] = Link{
-				Title: newId,
-				Link:  domain + "/?id=" + newId,
-			}
-		}
-
-		err = tmpl.Execute(w, links)
-		if err != nil {
-			panic("Failed to execute template")
-		}
-	} else {
+	idString := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		// id is not an int. Create error page
 		_, err := fmt.Fprintf(w, "Invalid id")
 		if err != nil {
 			panic("Failed to write body")
 		}
+	} else {
+		// Create page
+		links := linkMap[id]
+		err = tmpl.Execute(w, links)
+		if err != nil {
+			panic("Failed to execute template")
+		}
 	}
 }
 
+func loadGraph(filename string) error {
+	// Open the file
+	csvfile, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	reader := csv.NewReader(bufio.NewReader(csvfile))
+
+	edges := 0
+	for {
+		// Read the values of one line the graph
+		values, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		// Parse the values
+		from, to := values[0], values[1]
+		fromId, err := strconv.Atoi(from)
+		if err != nil {
+			return err
+		}
+		_, err = strconv.Atoi(to)
+		if err != nil {
+			return err
+		}
+
+		// Construct link
+		link := Link{
+			Title: to,
+			Link:  domain + "/?id=" + to,
+		}
+		pageLinks := linkMap[fromId]
+		pageLinks = append(pageLinks, link)
+		linkMap[fromId] = pageLinks
+		edges++
+	}
+
+	fmt.Printf("%s was loaded succesfully (%d edges)\n", filename, edges)
+	return nil
+}
+
 func main() {
-	fmt.Println("vim-go")
+	err := loadGraph("graph.csv")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting server..")
 	http.HandleFunc("/", pageHandler)
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
